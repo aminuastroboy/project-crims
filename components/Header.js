c9bf5336845e9d0b3be4a57ac5226fab97e03b04
Aminu Muhammad <aminuastroboy@gmail.com>
2026-02-19 22:37:11 +0100

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,1 +1,21 @@
+// src/components/Header.js
+import React from 'react';
 
+function Header() {
+    return (
+        <header>
+            <nav>
+                <div className="logo">
+                    <h1>CRIMS</h1>
+                    <span>Crime Records & Intelligence</span>
+                </div>
+                <div className="user">
+                    <span>Welcome, Inspector Adebayo</span>
+                    <button>Logout</button>
+                </div>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
