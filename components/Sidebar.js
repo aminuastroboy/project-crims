e9913655cd84be14558410dbe12016a9dd12caf9
Aminu Muhammad <aminuastroboy@gmail.com>
2026-02-19 22:38:05 +0100

    Add Sidebar component with navigation links

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,1 +1,18 @@
+// src/components/Sidebar.js
+import React from 'react';
+import { Link } from 'react-router-dom';
 
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <ul>
+                <li><Link to="/dashboard">Dashboard</Link></li>
+                <li><Link to="/suspects">Suspects</Link></li>
+                <li><Link to="/cases">Cases</Link></li>
+                <li><Link to="/analytics">Analytics</Link></li>
+            </ul>
+        </div>
+    );
+}
+
+export default Sidebar;
