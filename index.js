6cb45ded474b2bbce70dc97248190d8e5832b097
Aminu Muhammad <aminuastroboy@gmail.com>
2026-02-19 21:53:03 +0100

    Wrap App component with Router for routing

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css';
+import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById('root')
+);
